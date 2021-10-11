1249964aa8c3e339a86f6a5c8d4641e276140d4d
William Carvalho <williamcoelho.noorden@sulamerica.com.br>
2021-10-11 18:48:53 -0300

    monoteste

--- src/test/java/com/acwilliam/reactive/test/MonoTest.java
+++ src/test/java/com/acwilliam/reactive/test/MonoTest.java
@@ -2,12 +2,37 @@ package com.acwilliam.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
+/*
+ * Reactive Streams
+ * 1. assicrono
+ * 2. não blocante
+ * 3. backpressure
+ * publisher <- (subscribe) subscriber - vai emitir os eventos ele é cold
+ * subscription é criado quando voce da o subscriber.
+ * o publisher (vai chamar o onSubscribe com o  subscription) -> Subscriber
+ * Subscription <-(request N) Subscriber
+ * Publisher -> OnNext) Subscriber
+ * executar até:
+ * 1. Publisher envia todos os objetos requisitado
+ * 2. Publisher envia todos os objetos. (onComplete) subscriber and e subscription são cancelados
+ * 3. Quando há um ero. (onError)-> subscriber e subscription são cancelados
+ */
 public class MonoTest {
 
     @Test
-    public void test(){
-        log.info("Sucess working");
+    public void monoSubscriber(){
+        String nome = "William Coelho";
+        Mono<String> mono = Mono.just(nome)
+                .log();
+        mono.subscribe();
+        log.info("---------------");
+        StepVerifier.create(mono)
+                .expectNext(nome)
+                .verifyComplete();
+
     }
 }
