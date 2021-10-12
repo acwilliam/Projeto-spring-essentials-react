c24e2a84cd1c25a7f66e56a3cbfd4717bea77dad
William Carvalho <williamcoelho.noorden@sulamerica.com.br>
2021-10-11 21:49:38 -0300

    fluxSubscriberNumbersNotSoUglyBackPressure

--- src/test/java/com/acwilliam/reactive/test/FluxTest.java
+++ src/test/java/com/acwilliam/reactive/test/FluxTest.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
@@ -78,7 +79,7 @@ public class FluxTest {
         flux.subscribe(new Subscriber<>() {
             private int count= 0;
             private Subscription subscription;
-            private int requestCount= 2;
+            private final int requestCount= 2;
 
             @Override
             public void onSubscribe(Subscription subscription) {
@@ -113,4 +114,37 @@ public class FluxTest {
     }
 
 
-}
+    @Test
+    public void fluxSubscriberNumbersNotSoUglyBackPressure(){
+        Flux<Integer> flux= Flux.range(1,10)
+                .log();
+
+        flux.subscribe(new BaseSubscriber<>() {
+            private int count= 0;
+            private final int requestCount= 2;
+
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(requestCount);
+            }
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                count++;
+                if(count >= requestCount){
+                    count =0;
+                    request(requestCount);
+            }
+          }
+
+        });
+
+        log.info("-------------------------------------");
+        StepVerifier.create(flux)
+                .expectNext(1,2,3,4,5,6,7,8,9,10)
+                .verifyComplete();
+    }
+
+
+
+    }
